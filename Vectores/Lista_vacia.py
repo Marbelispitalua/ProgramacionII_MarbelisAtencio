5f12ec5159d071e9eb78b08e0c57b60e9b133356
Marbelispitalua <160240688+Marbelispitalua@users.noreply.github.com>
2024-03-22 08:39:55 -0500

    Add files via upload

---
+++ Vectores/Lista_vacia.py
@@ -0,0 +1,17 @@
+Lista_vacia = []
+
+Lista = ["Marbelis" , "Atencio" , "18años" , "1063145199" , "Lorica_Cordoba"]
+
+print(len(Lista))
+
+print("La longitud de la lista es:" , len(Lista))
+
+print("El primer elemento de la lista es:" , Lista[0])
+print("El tercer elemento de la lista es:" , Lista[2])
+print("El ultimo elemento de la lista es:" , Lista[4])
+
+Datos_personales = ['Marbelis' , '18' , '1.57' , 'Soltera' , 'Barrio_Boston']
+
+Datos_personales.append()
+
+print(Datos_personales)
